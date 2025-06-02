82dde0a16f482281f427f732be2da74c33da3813
Website <yucharles254@gmail.com>
2025-06-02 18:04:38 +0800

    Initial Commit

---
+++ clock/clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by Charles on 2/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
